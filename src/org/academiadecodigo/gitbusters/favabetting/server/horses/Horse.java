b55df9e181e12e6d2260fa46cbe3bfc18a3cf75d
Vitorsly <vitorsly@sapo.pt>
2020-07-04 00:24:49 +0000

    Merge fixed

--- src/org/academiadecodigo/gitbusters/favabetting/server/horses/Horse.java
+++ src/org/academiadecodigo/gitbusters/favabetting/server/horses/Horse.java
@@ -9,13 +9,16 @@ public class Horse {
     private int wins;
     private int races;
     private boolean alive;
-    private RaceStrategy strategy;
+    private int maxSpeed;
+    private double distance;
 
-    Horse(int id, String name, String description, int speed, double odds, int wins, int races){
+    Horse(int id, String name, String description, int speed, int maxSpeed, double odds, int wins, int races){
         this.id = id;
         this.description = description;
         this.name = name;
         this.speed = speed;
+        this.maxSpeed = maxSpeed;
+        distance = 0;
         this.odds = odds;
         this.wins = wins;
         this.races = races;
@@ -50,24 +53,8 @@ public class Horse {
         alive = false;
     }
 
-    public void setStrategy(RaceStrategy strategy) {
-        this.strategy = strategy;
-    }
-
     public void shiftSpeed(int speedModifier){
         this.speed += speedModifier;
-    }
-
-    private String name;
-    public double speed;
-    private int maxSpeed;
-    private double distance;
-
-    public Horse(String name, double speed, int maxSpeed) {
-        this.name = name;
-        this.speed = speed;
-        this.maxSpeed = maxSpeed;
-        this.distance = 0;
     }
 
     public void race() {
@@ -78,17 +65,9 @@ public class Horse {
         return distance;
     }
 
-    public void setSpeed(double value) {
+    public void setSpeed(int value) {
         if(value <= maxSpeed) {
             this.speed = value;
         }
-    }
-
-    public double getSpeed() {
-        return this.speed;
-    }
-
-    public String getName() {
-        return this.name;
     }
 }
