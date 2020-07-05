e50393c51982853e7c8f1f0ba0139cbfa6337e7f
Vitorsly <vitorsly@sapo.pt>
2020-07-05 17:34:41 +0000

    Added BigDescriptions and slight rebalancing

--- src/org/academiadecodigo/gitbusters/favabetting/server/cheats/BaseballBat.java
+++ src/org/academiadecodigo/gitbusters/favabetting/server/cheats/BaseballBat.java
@@ -7,13 +7,13 @@ public class BaseballBat extends AbstractCheat{
     public void init(){
         price = 80;
         fine = 200;
-        policeChance = 10;
+        policeChance = 15;
         name = "Baseball Bat";
         description = "This here bat has done some serious damage to some horse legs. Permanent serious damage to a horse's winning chances.";
     }
 
     @Override
     public void activate(Horse target) {
-        target.permSpeedChange(-10);
+        target.permSpeedChange(-15);
     }
 }
